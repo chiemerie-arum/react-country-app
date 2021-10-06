a4d4d63204e4fe152ffc432c0cd5eef15a667ce6
Adam Rousseau <adam.rousseau@aboutyou.com>
2021-10-06 22:10:04 +0200

    implicit return

--- src/components/CountryCard/index.tsx
+++ src/components/CountryCard/index.tsx
@@ -1,25 +1,17 @@
 import { Country } from "../../App";
-import React from "react";
-import './style.scss'
+import "./style.scss";
 
+const CountryCard = (country: Country) => (
+  <article className="country__container">
+    <div className="country__summary">
+      <p className="country__name">
+        <span className="country__alpha"> {country.alpha2Code} </span>
+        {country.name}
+      </p>
+      <p className="country__capital">{country.capital}</p>
+    </div>
+    <img className="country__flag" src={country.flag} alt="" />
+  </article>
+);
 
-const CountryCard = (country: Country) => {
-
-    return (
-        <article className="country__container">
-            <div className="country__summary">
-                <p className="country__name">
-                    <span className="country__alpha"> {country.alpha2Code} </span>
-                    {country.name}
-                </p>
-                <p className="country__capital">{country.capital}</p>
-            </div>
-
-            <img className="country__flag"
-                src={country.flag} alt="" />
-        </article>
-    )
-
-}
-
-export default CountryCard
+export default CountryCard;
--- src/utils/services/CountryService.ts
+++ src/utils/services/CountryService.ts
@@ -1,26 +1,26 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'https://restcountries.com/v2',
-    withCredentials: false,
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    }
-  });
+  baseURL: "https://restcountries.com/v2",
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
 
- const CountryService = {
-    getCountries() {
-      return apiClient('/all');
-    },
-  
-    getCountry(country: string) {
-      return apiClient(`/name/${country}`);
-    },
-  
-    getCountryByCode(code: string) {
-      return apiClient(`/alpha/${code}`);
-    }
-  };
+const CountryService = {
+  getCountries() {
+    return apiClient("/all");
+  },
 
-  export default CountryService
+  getCountry(country: string) {
+    return apiClient(`/name/${country}`);
+  },
+
+  getCountryByCode(code: string) {
+    return apiClient(`/alpha/${code}`);
+  },
+};
+
+export default CountryService;
